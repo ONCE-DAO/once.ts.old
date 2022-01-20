c5e4cf94b49f2a339c0bf316fb4aeb4aa4a6c055
phibar <philippbartels@outlook.com>
2022-01-20 17:56:02 +0100

    added once html

--- src/2_systems/Once.class.ts
+++ src/2_systems/Once.class.ts
@@ -1,101 +1,121 @@
-import { NodeLoader } from './../unsorted/NodeLoader'
-import { Once as OnceInterface, OnceInstallationMode, OnceMode, OnceState } from '../3_services/Once.interface'
-import { Server } from '../3_services/Server.interface'
+import { NodeLoader } from "./../unsorted/NodeLoader";
+import {
+  Once as OnceInterface,
+  OnceInstallationMode,
+  OnceMode,
+  OnceState,
+} from "../3_services/Once.interface";
+import { Server } from "../3_services/Server.interface";
 // import { NodeLoader } from '../unsorted/NodeLoader'
 
-let load, resolve
+let load, resolve;
 declare global {
-  // eslint-disable-next-line no-var
-  var ONCE: Once | undefined
+  var ONCE: Once | undefined;
 }
 
 class Once implements OnceInterface {
-  server: Server[] = []
+  server: Server[] = [];
 
-  private creationDate: number
+  private creationDate: number;
 
-  private constructor () {
-    this.creationDate = Date.now()
+  private constructor() {
+    this.creationDate = Date.now();
   }
 
-  private onces: Once[] = []
-  private installationMode: OnceInstallationMode = OnceInstallationMode.TRANSIENT
-  private mode: OnceMode = OnceMode.BOOTING
-  private state: OnceState = OnceState.DISCOVER
+  private onces: Once[] = [];
+  private installationMode: OnceInstallationMode =
+    OnceInstallationMode.TRANSIENT;
+  private mode: OnceMode = OnceMode.BOOTING;
+  private state: OnceState = OnceState.DISCOVER;
 
-  static async start () {
-    const once = new Once()
-    await once.discover()
-    console.log('ONCE Started', once)
+  static async start() {
+    const once = new Once();
+    await once.discover();
+    console.log("ONCE Started", once);
   }
 
-  async discover () {
+  async discover() {
+    this.onces.push(this);
+
     if (Once.isNodeLoader) {
-      const nodeLoader = NodeLoader.start(this)
-      this.mode = OnceMode.NODE_LOADER
-      this.state = OnceState.STARTED
+      const nodeLoader = NodeLoader.start(this);
+      this.mode = OnceMode.NODE_LOADER;
+      this.state = OnceState.STARTED;
 
-      load = nodeLoader.load
-      resolve = nodeLoader.resolve
+      load = nodeLoader.load;
+      resolve = nodeLoader.resolve;
+      global.ONCE = this;
     }
 
     if (Once.isNode) {
-      if (global.ONCE) this.onces.push(global.ONCE)
-      this.mode = OnceMode.NODE_JS
-      // @ts-ignore
-      const OnceExpress = (await import('./../../../once.express@main/src/2_systems/OnceExpress.js')).OnceExpress
-      OnceExpress.start()
-      this.state = OnceState.STARTED
+      if (global.ONCE) this.onces.push(global.ONCE);
+      this.mode = OnceMode.NODE_JS;
+      const OnceExpress = (
+        await import(
+          // @ts-ignore
+          "./../../../once.express@main/src/2_systems/OnceExpress.js"
+        )
+      ).OnceExpress;
+      OnceExpress.start();
+      this.state = OnceState.STARTED;
+      global.ONCE = this;
     }
 
-    this.onces.push(this)
-    global.ONCE = this
-    return []
+    if (Once.isBrowser) {
+      this.mode = OnceMode.BROWSER;
+      this.state = OnceState.STARTED;
+      console.log("ONCE STARTED IN BROWSER");
+      window.ONCE = this;
+      document.body.textContent = "Once started"
+    }
+
+    console.log("Once discovered: ", ONCE);
+    return [];
   }
 
-  private static get isBrowser () {
+  private static get isBrowser() {
     return (
-      typeof window !== 'undefined' && typeof window.document !== 'undefined'
-    )
+      typeof window !== "undefined" && typeof window.document !== "undefined"
+    );
   }
 
-  private static get isNodeLoader () {
-    return Once.isNodeRuntime && global.ONCE === undefined
+  private static get isNodeLoader() {
+    return Once.isNodeRuntime && global.ONCE === undefined;
   }
 
-  private static get isNode () {
-    return Once.isNodeRuntime && global.ONCE !== undefined
+  private static get isNode() {
+    return Once.isNodeRuntime && global.ONCE !== undefined;
   }
 
-  private static get isNodeRuntime () {
+  private static get isNodeRuntime() {
     return (
-      typeof process !== 'undefined' &&
+      typeof process !== "undefined" &&
       process.versions != null &&
       process.versions.node != null
-    )
+    );
   }
 
-  private static get isWebWorker () {
+  private static get isWebWorker() {
     return (
-      typeof self === 'object' &&
+      typeof self === "object" &&
       self.constructor &&
-      self.constructor.name === 'DedicatedWorkerGlobalScope'
-    )
+      self.constructor.name === "DedicatedWorkerGlobalScope"
+    );
   }
 
-  private static get isServiceWorker () {
+  private static get isServiceWorker() {
     return (
-      typeof self === 'object' &&
+      typeof self === "object" &&
       self.constructor &&
-      self.constructor.name === 'ServiceWorkerGlobalScope'
-    )
+      self.constructor.name === "ServiceWorkerGlobalScope"
+    );
   }
 
-  private static isServiceWorkerSupported () {
-    return navigator && 'serviceWorker' in navigator
+  private static isServiceWorkerSupported() {
+    return navigator && "serviceWorker" in navigator;
   }
 }
 
-Once.start()
+Once.start();
 
-export { load, resolve, Once }
+export { load, resolve, Once };
