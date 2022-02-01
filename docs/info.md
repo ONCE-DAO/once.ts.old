cd74cac9481fe652bfcdf660e05d4ad675cafbe7
phibar <philippbartels@outlook.com>
2022-02-01 09:48:12 +0100

    update submodule logic

--- docs/info.md
+++ docs/info.md
@@ -1,2 +1,4 @@
 # some resources 
 https://www.freecodecamp.org/news/you-should-never-ever-run-directly-against-node-js-in-production-maybe-7fdfaed51ec6/
+
+https://www.twilio.com/blog/how-to-build-a-cli-with-node-js
--- src/unsorted/Submodule.ts
+++ src/unsorted/Submodule.ts
@@ -4,16 +4,30 @@ import fs from "fs";
 import { Package } from "./Package";
 import { OnceBuilder } from "./OnceBuilder.class";
 export class Submodule {
-  static async addFromUrl(url: string, branch?: string) {
-    const root = ONCE?.directory || "";
+  static async addFromUrl(
+    url: string,
+    branch?: string,
+    overwrite?: { name: string; namespace: string }
+  ) {
+    // const root = ONCE?.directory || "";
+    const root = "/Users/philippbartels/EAMD.ucp";
     const tmpFolder = path.join(root, "tmp");
     !fs.existsSync(tmpFolder) && fs.mkdirSync(tmpFolder);
+    const pkg = await Package.getByPath(path.join(tmpFolder, "package.json"));
+
+    if (overwrite && pkg) {
+      pkg.name = overwrite.name;
+      pkg.namespace = overwrite.namespace;
+      fs.writeFileSync(
+        path.join(tmpFolder, "package.json"),
+        JSON.stringify(pkg, null, 2)
+      );
+    }
 
     const repo = await GitRepository.start({
       baseDir: path.join(root, "tmp"),
       clone: { url, branch },
     });
-    const pkg = await Package.getByPath(path.join(tmpFolder, "package.json"));
     const split = pkg?.namespace?.split(".");
     const packageFolder = split ? split : ["empty"];
     const componentFolder = path.join(
