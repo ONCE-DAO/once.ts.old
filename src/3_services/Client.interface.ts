93858711e98bb0bb069050736952090ac0420f04
phibar <philippbartels@outlook.com>
2022-02-01 20:19:26 +0100

    full reset

--- src/3_services/Client.interface.ts
+++
@@ -1,5 +0,0 @@
-import { Server } from './Server.interface'
-
-export interface Client {
-    server: Server
-}
--- src/3_services/Thing.interface.ts
+++
@@ -1,4 +0,0 @@
-import { IOR } from './IOR.interface'
-export interface Thing {
-    discover(): Promise<IOR[]>
-}
