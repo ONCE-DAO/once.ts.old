8ce7f8bbf263941ac03665588b2bb11fbfc83674
phibar <philippbartels@outlook.com>
2022-02-04 14:14:00 +0100

    ganz schön dumm

--- src/2_systems/Once.class.ts
+++ src/2_systems/Once.class.ts
@@ -5,10 +5,4 @@ export {
   globalPreload,
 } from "./Once/OnceNodeImportLoader.class";
 
-if (global.started !== undefined && !global.started) {
-  global.started = true;
-  await OnceKernel.start();
-}
-else {
-    console.log("sdsd")
-}
+await OnceKernel.start();
