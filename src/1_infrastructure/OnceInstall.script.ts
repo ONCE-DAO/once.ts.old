9aa2fd14529597618d92ee494eee0e198018a68e
phibar <philippbartels@outlook.com>
2022-02-06 04:47:48 +0100

    bugfix install script

--- src/1_infrastructure/OnceInstall.script.ts
+++ src/1_infrastructure/OnceInstall.script.ts
@@ -1,2 +1,14 @@
-(await import("../2_systems/Once/OnceNodeServer.class.js")).default.initEAMD();
+import { RootEAMD } from "../2_systems/EAMD/RootEAMD.class.js";
+import { UserEAMD } from "../2_systems/EAMD/UserEAMD.class.js";
+
+const rootEAMD = RootEAMD.getInstance().init();
+if (rootEAMD.hasWriteAccess()) {
+  if (!rootEAMD.isInstalled()) await rootEAMD.install();
+} else {
+  const userEAMD = UserEAMD.getInstance().init();
+  if (userEAMD.hasWriteAccess()) {
+    if (!userEAMD.isInstalled()) await userEAMD.install();
+  }
+}
+
 export {};
