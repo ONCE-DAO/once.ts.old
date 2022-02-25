80e402d5f65c394eeaa28b7bbd0107bda697a3a8
Marcel Donges <marcel@donges.it>
2022-02-25 18:23:11 +0100

    better naming; narrowing not working

--- src/1_infrastructure/BaseThing.class.ts
+++ src/1_infrastructure/BaseThing.class.ts
@@ -4,7 +4,7 @@ import ClassDescriptor from "../3_services/ClassDescriptor.interface";
 import WeakRefPromiseStore from "../2_systems/Things/WeakRefPromiseStore.class";
 import Store from "../3_services/Store.interface";
 import Class from "../3_services/Class.interface";
-import { Metaclass, TSClass } from "../3_services/TypeDescriptor.interface";
+import { Metaclass, TSClass } from '../3_services/TypeDescriptor.interface';
 
 export default abstract class BaseThing<T> implements Thing<T> {
 
@@ -27,24 +27,29 @@ export default abstract class BaseThing<T> implements Thing<T> {
   }
 
 
-  static get class(): Metaclass | undefined {
+  static get type(): Metaclass {
     return Metaclass.getClass(this);
-    let result = this._typeDescriptorStore.get(this);
-    if (!result) {
-      // @ts-ignore
-      // It is abstract, but TS does not understand that
-      result = new DefaultClassDescriptor().init(this);
-      this._typeDescriptorStore.set(this, result);
-    }
-    return result;
+    // let result = this._typeDescriptorStore.get(this);
+    // if (!result) {
+    //   // @ts-ignore
+    //   // It is abstract, but TS does not understand that
+    //   result = new DefaultClassDescriptor().init(this);
+    //   this._typeDescriptorStore.set(this, result);
+    // }
+    // return result;
   }
 
-  get class(): Metaclass | undefined {
+  get type(): Metaclass {
     //TODO@MD Check how to do it better
     // @ts-ignore
-    return this.constructor.classDescriptor;
+    return (this.constructor as Metaclass).type;
   }
-
+  
+  get tsClass(): TSClass {
+    //TODO@MD Check how to do it better
+    // @ts-ignore
+    return Metaclass.getClass(this) as TSClass;
+  }
 
 
   get name(): string { return this.constructor.name };
